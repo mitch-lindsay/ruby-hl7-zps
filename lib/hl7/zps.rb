5113ab2bdb4ce1007ba68dc41f81adc438d981f0
Enrique Carlos Mogollan <emogollan@gmail.com>
2015-07-24 17:07:16 -0700

    Adding custom ZPS segment

--- lib/hl7/zps.rb
+++ lib/hl7/zps.rb
@@ -1,6 +1,16 @@
 require "ruby/hl7/zps/version"
 
 module HL7
-  class Message::Segment::ZPS
+  class Message::Segment::ZPS < HL7::Message::Segment
+    weight 12
+    add_field :set_id,                  :idx => 1
+    add_field :facility_footnote_code,  :idx => 2
+    add_field :facility_name,           :idx => 3
+    add_field :facility_address,        :idx => 4
+    add_field :facility_phone_number,   :idx => 5
+    add_field :facility_contact,        :idx => 6
+    add_field :facility_director,       :idx => 7
+    add_field :facility_lab_code,       :idx => 8
+    add_field :facility_clia_number,    :idx => 9
   end
 end
